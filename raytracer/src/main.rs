46eaeb4bfecdf7534e6eb42cd7401e532181b9ca
AegeanYan <1203616626@qq.com>
2021-07-15 09:17:29 +0800

    d第一本书暂时完成

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -1,26 +1,304 @@
 #[allow(clippy::float_cmp)]
 mod vec3;
+mod hit;
+mod camera;
+mod rtweekend;
+mod material;
+
 use image::{ImageBuffer, RgbImage};
-use indicatif::ProgressBar;
-
 pub use vec3::Vec3;
+pub use vec3::Ray;
+pub use rtweekend::*;
+use std::f64::consts::PI;
+use crate::hit::{HittableList, Sphere, Hittable, HitRecord};
+use crate::material::{Material, Lambertian ,Metal,Dielectric};
+use crate::camera::Camera;
+use std::sync::Arc;
+// fn main() {
+//     let x = Vec3::new(1.0, 1.0, 1.0);
+//     println!("{:?}", x);
+//
+//     let mut img: RgbImage = ImageBuffer::new(256, 256);
+//     let bar = ProgressBar::new(256);
+//
+//     for j in (0..256) {
+//         for i in 0..256 {
+//             let pixel = img.get_pixel_mut(i, j);
+//             // let color = (x / 4) as u8;
+//             let r:f64 = (i as f64/ 255.0) ;
+//             let g:f64 = ((256 - j) as f64/ 255.0) ;
+//             let b:f64 = 0.25;
+//             // let ir:u8 = (255.999 * r) as u8;
+//             // let ig:u8 = (255.999 * g) as u8;
+//             // let ib:u8 = (255.999 * b) as u8;
+//             // *pixel = image::Rgb([ir , ig , ib]);
+//             *pixel = write_color(&r , &g , &b);
+//         }
+//         //bar.inc(7);
+//     }
+//
+//     img.save("output/test.png").unwrap();
+//     bar.finish();
+// }
 
-fn main() {
-    let x = Vec3::new(1.0, 1.0, 1.0);
-    println!("{:?}", x);
+fn main(){
 
-    let mut img: RgbImage = ImageBuffer::new(1024, 512);
-    let bar = ProgressBar::new(1024);
 
-    for x in 0..1024 {
-        for y in 0..512 {
-            let pixel = img.get_pixel_mut(x, y);
-            let color = (x / 4) as u8;
-            *pixel = image::Rgb([color, color, color]);
+    //Image
+    const ASPECT_RATIO:f64 = 16.0/9.0;
+    const IMAGE_WIDTH:u32 = 400;
+    const IMAGE_HEIGHT:u32 = (IMAGE_WIDTH as f64/ ASPECT_RATIO) as u32;
+    const SAMPLES_PER_PIXEL: u32 = 100;
+    const MAX_DEPTH:u32 = 50;
+    let mut img: RgbImage = ImageBuffer::new(IMAGE_WIDTH, IMAGE_HEIGHT);
+    //World
+    let mut world:HittableList = HittableList { objects: vec![] };
+    let mg:Lambertian = Lambertian{
+        albedo: Vec3 {
+            x: 0.8,
+            y: 0.8,
+            z: 0.0
         }
-        bar.inc(1);
-    }
+    };
+    let material_ground:Sphere = Sphere{
+        center: Vec3 {
+            x: 0.0,
+            y: -100.5,
+            z: -1.0
+        },
+        radius: 100.0,
+        mat_ptr: Arc::new(mg),
+    };
 
+    world.add(Arc::new(material_ground));
+    let mc:Lambertian = Lambertian{
+        albedo: Vec3 {x:0.1 , y:0.2 , z:0.5},
+    };
+    let material_center:Sphere = Sphere{
+        center: Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: -1.0
+        },
+        radius: 0.5,
+        mat_ptr: Arc::new(mc),
+    };
+    world.add(Arc::new(material_center));
+    let ml:Dielectric = Dielectric{
+        ref_idx: 1.5,
+    };
+    let material_left:Sphere = Sphere{
+        center: Vec3 {
+            x: -1.0,
+            y: 0.0,
+            z: -1.0
+        },
+        radius: 0.5,
+        mat_ptr: Arc::new(ml),
+    };
+    let material_left1:Sphere = Sphere{
+        center: Vec3 {
+            x: -1.0,
+            y: 0.0,
+            z: -1.0
+        },
+        radius: -0.45,
+        mat_ptr: Arc::new(ml),
+    };
+
+    world.add(Arc::new(material_left));
+    world.add(Arc::new(material_left1));
+    let mr:Metal = Metal{
+        albedo: Vec3 {
+            x: 0.8,
+            y: 0.6,
+            z: 0.2
+        },
+        fuzz: 0.0,
+    };
+    let material_right:Sphere = Sphere{
+        center: Vec3 {
+            x: 1.0,
+            y: 0.0,
+            z: -1.0
+        },
+        radius: 0.5,
+        mat_ptr: Arc::new(mr),
+    };
+
+    world.add(Arc::new(material_right));
+    //四球例子
+
+    // let R:f64 = (PI / 4.0).cos();
+    // let ml:Lambertian = Lambertian{
+    //     albedo: Vec3 {x:0.0 , y:0.0 , z:1.0},
+    // };
+    // let material_left:Sphere = Sphere{
+    //     center: Vec3 {x:-R , y:0.0 ,z:-1.0},
+    //     radius: R,
+    //     mat_ptr: Arc::new(ml),
+    // };
+    // world.add(Arc::new(material_left));
+    //
+    // let mr:Lambertian = Lambertian{
+    //     albedo: Vec3 {x:1.0 , y:0.0 , z:0.0},
+    // };
+    // let material_right:Sphere = Sphere{
+    //     center: Vec3 {x:R , y:0.0 , z:-1.0},
+    //     radius: R,
+    //     mat_ptr: Arc::new(mr)
+    // };
+    // world.add(Arc::new(material_right));
+    //双球例子
+
+    // let sph1 = Sphere{
+    //     center: Vec3 {
+    //         x: 0.0,
+    //         y: 0.0,
+    //         z: -1.0,
+    //     },
+    //     radius: 0.5,
+    // };
+    // let sph2 = Sphere{
+    //     center: Vec3 {
+    //         x: 0.0,
+    //         y: -100.5,
+    //         z: -1.0,
+    //     },
+    //     radius: 100.0,
+    // };
+    // world.add(Box::new(sph2));
+    // world.add(Box::new(sph1));
+
+    //Camera
+    // let viewport_height:f64 = 2.0;
+    // let viewport_width:f64 = ASPECT_RATIO * viewport_height;
+    // let focal_length = 1.0;
+    //
+    // let origin:Vec3 = Vec3::new(0.0 , 0.0 ,0.0);
+    // let horizontal:Vec3 = Vec3::new(viewport_width , 0.0 , 0.0);
+    // let vertical:Vec3 = Vec3::new(0.0 , viewport_height , 0.0);
+
+    // let cam:Camera = Camera::new(); //四球
+    let lookfrom = Vec3::new(3.0 , 3.0 , 2.0);
+    let lookat = Vec3::new(0.0 , 0.0 , -1.0);
+    let vup = Vec3::new(0.0 , 1.0 , 0.0);
+    let dist_to_focus = (lookfrom - lookat).length();
+    let aperture = 2.0;
+    let cam:Camera = Camera::camera_from_where(lookfrom , lookat , vup , 20.0 , 16.0 / 9.0 , aperture , dist_to_focus);
+
+    // //视口左下角的坐标
+    // let lower_left_corner:Vec3 = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0 , 0.0 , focal_length);
+
+    //Render
+    for j in (0 .. IMAGE_HEIGHT).rev(){
+        for i in 0 .. IMAGE_WIDTH {
+            let mut pixel_color:Vec3 = Vec3::new(0.0 , 0.0 , 0.0);
+            let pixel = img.get_pixel_mut(i, IMAGE_HEIGHT - 1 - j);
+            for k in 0..SAMPLES_PER_PIXEL {
+                let u:f64 = (i as f64 + random_double()) / ((IMAGE_WIDTH - 1) as f64);
+                let v:f64 = (j as f64 + random_double()) / ((IMAGE_HEIGHT - 1) as f64);
+                let r:Ray = cam.get_ray(u , v);
+                pixel_color += ray_color(r , &world ,MAX_DEPTH);
+            }
+            *pixel = write_color(&pixel_color , SAMPLES_PER_PIXEL);
+        }
+    }
     img.save("output/test.png").unwrap();
-    bar.finish();
+}
+fn write_color(pixel_color:&Vec3 , samples_per_pixel:u32) -> image::Rgb<u8>{
+    let mut r:f64 = pixel_color.x;
+    let mut g:f64 = pixel_color.y;
+    let mut b:f64 = pixel_color.z;
+
+    let scale:f64 = 1.0 / (samples_per_pixel as f64);
+    r = (r * scale).sqrt();
+    g = (g * scale).sqrt();
+    b = (b * scale).sqrt();
+
+    let ir:u8 = (256.0 * rtweekend::clamp(r , 0.0 , 0.999)) as u8;
+    let ig:u8 = (256.0 * rtweekend::clamp(g , 0.0 , 0.999)) as u8;
+    let ib:u8 = (256.0 * rtweekend::clamp(b , 0.0 , 0.999)) as u8;
+    return image::Rgb([ir , ig , ib]);
+}
+
+// fn ray_color(r:Ray) -> Vec3{
+//     let t:f64 = hit_sphere(Vec3::new(0.0 , 0.0 , -1.0) , 0.5 , &r);
+//     if t > 0.0 {
+//         let n:Vec3 = Vec3::unit_vector(r.at(t) - Vec3::new(0.0 , 0.0 , -1.0));
+//         return Vec3::new(n.x + 1.0, n.y + 1.0, n.z + 1.0) * 0.5;
+//     }
+//     let unit_direction:Vec3 = Vec3::unit_vector(r.dir);
+//     let t:f64 = 0.5 * (unit_direction.y + 1.0);
+//     return Vec3::new(1.0 , 1.0 , 1.0) * (1.0 - t) + Vec3::new(0.5 , 0.7 , 1.0) * t;
+// }
+//
+// fn hit_sphere(center:Vec3 , radius:f64 , r:&Ray)->f64{
+//     let oc:Vec3 = r.orig - center;
+//     let a:f64 = Vec3::dot(r.dir , r.dir);
+//     let half_b:f64 = Vec3::dot(r.dir , oc);
+//     let c:f64 = oc.length_squared() - radius * radius;
+//     let discriminant:f64 = half_b * half_b - a * c;
+//     if discriminant < 0.0 {
+//         return -1.0;
+//     }else {
+//         let t1:f64 = (-half_b - discriminant.sqrt()) /a;
+//         if t1 > 0.0 {
+//             return t1;
+//         }
+//         let t2:f64 = (-half_b + discriminant.sqrt()) /a;
+//         if t2 > 0.0 {
+//             return t2;
+//         }
+//         return -1.0;
+//     }
+// }
+
+fn ray_color(r:Ray, world: &dyn Hittable , depth:u32) -> Vec3{
+    let mut rec = HitRecord{
+        p: Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0
+        },
+        normal: Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0
+        },
+        mat_ptr: Arc::new(Metal::new()),
+        t: 0.0,
+        front_face: true,
+    };
+    if depth <= 0 {
+        return Vec3::new(0.0 , 0.0 , 0.0);
+    }
+    if world.hit(r , 0.001 , f64::INFINITY , &mut rec) {
+        let mut scattered: Ray = Ray {
+            orig: Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0
+            },
+            dir: Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0
+            }
+        };
+        let mut attenuation: Vec3 = Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0
+        };
+
+        if rec.mat_ptr.scatter(r , rec.clone() ,&mut attenuation ,&mut scattered) {
+            return ray_color(scattered , world , depth - 1) * attenuation;
+        }
+        return Vec3::new(0.0 , 0.0 ,0.0);
+    }
+    let unit_direction:Vec3 = Vec3::unit_vector(r.dir);
+    let t:f64 = 0.5 * (unit_direction.y + 1.0);
+    //线性插值
+    return Vec3::new(1.0 , 1.0 , 1.0) * (1.0 - t) + Vec3::new(0.5 , 0.7 , 1.0) * t;
 }
--- raytracer/src/vec3.rs
+++ raytracer/src/vec3.rs
@@ -1,10 +1,24 @@
-use std::ops::{Add, AddAssign};
+use std::ops::{Add, AddAssign, Sub ,
+               SubAssign , Mul , MulAssign ,  Div , DivAssign
+};
+use crate::{random_double, random_double_lim};
+use std::f64::consts::PI;
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, Copy)]
 pub struct Vec3 {
     pub x: f64,
     pub y: f64,
     pub z: f64,
+}
+#[derive(Clone , Debug , PartialEq , Copy)]
+pub struct Ray {
+    pub orig:Vec3,
+    pub dir:Vec3,
+}
+impl Ray{
+    pub fn at(&self , t:f64)->Vec3{
+        return self.orig + self.dir * t;
+    }
 }
 
 impl Vec3 {
@@ -59,6 +73,235 @@ impl AddAssign for Vec3 {
     }
 }
 
+impl AddAssign<f64> for Vec3 {
+    fn add_assign(&mut self, other: f64) {
+        *self = Self {
+            x: self.x + other,
+            y: self.y + other,
+            z: self.z + other,
+        };
+    }
+}
+
+impl Sub for Vec3{
+    type Output = Self;
+
+    fn sub(self , other:Self) -> Self{
+        Self{
+            x:self.x - other.x,
+            y:self.y - other.y,
+            z:self.z - other.z,
+        }
+    }
+}
+
+impl Sub<f64> for Vec3{
+    type Output = Self;
+
+    fn sub(self , other:f64) -> Self{
+        Self{
+            x:self.x - other,
+            y:self.y - other,
+            z:self.z - other,
+        }
+    }
+}
+
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        };
+    }
+}
+
+impl SubAssign<f64> for Vec3 {
+    fn sub_assign(&mut self, other: f64) {
+        *self = Self {
+            x: self.x - other,
+            y: self.y - other,
+            z: self.z - other,
+        };
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        }
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: f64) -> Self {
+        Self {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        }
+    }
+}
+
+impl MulAssign for Vec3 {
+    fn mul_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        };
+    }
+}
+
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, other: f64) {
+        *self = Self {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        };
+    }
+}
+
+impl Div for Vec3 {
+    type Output = Self;
+
+    fn div(self, other: Self) -> Self {
+        Self {
+            x: self.x / other.x,
+            y: self.y / other.y,
+            z: self.z / other.z,
+        }
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Self;
+
+    fn div(self, other: f64) -> Self {
+        Self {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other,
+        }
+    }
+}
+
+impl DivAssign for Vec3 {
+    fn div_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x / other.x,
+            y: self.y / other.y,
+            z: self.z / other.z,
+        };
+    }
+}
+
+impl DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, other: f64) {
+        *self = Self {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other,
+        };
+    }
+}
+
+impl Vec3{
+    pub fn dot(u:Vec3 , v:Vec3)->f64{
+        return u.x * v.x + u.y * v.y + u.z * v.z;
+    }
+
+    pub fn cross(u:Vec3 , v:Vec3)->Vec3{
+        return Vec3::new(u.y * v.z - u.z * v.y , u.z * v.x - u.x * v.z , u.x * v.y - u.y * v.x);
+    }
+
+    pub fn length_squared(&self)->f64{
+        return self.x * self.x + self.y * self.y + self.z * self.z;
+    }
+    pub fn length(&self)->f64{
+        let sqe = Vec3::length_squared(&self);
+        return sqe.sqrt() as f64;
+    }
+    pub fn unit_vector(v:Vec3) ->Vec3{
+        let divs = Vec3::length(&v);
+        return v / divs;
+    }
+
+    pub fn unit(&mut self){
+        let divs = Vec3::length(&self);
+        *self = Self{
+            x : self.x / divs,
+            y : self.y / divs,
+            z : self.z / divs,
+        }
+    }
+}
+
+impl Vec3{
+    pub fn random()->Vec3{
+        return Vec3::new(random_double() , random_double() , random_double());
+    }
+
+    pub fn random_limit(min:f64 , max:f64)->Vec3{
+        return Vec3::new(random_double_lim(min , max) , random_double_lim(min , max) , random_double_lim(min , max));
+    }
+    pub fn random_in_unit_sphere() -> Vec3{
+        while true {
+            let p:Vec3 = Vec3::random_limit(-1.0 , 1.0);
+            if p.length_squared() >= 1.0 {
+                continue;
+            }
+            return p;
+        }
+        return Vec3::new(0.0 , 0.0 , 0.0);
+    }
+
+    pub fn random_unit_vector() -> Vec3{
+        let a:f64 = random_double_lim(0.0 , 2.0 * PI);
+        let z:f64 = random_double_lim(-1.0 , 1.0);
+        let r:f64 = (1.0 - z * z).sqrt();
+        return Vec3::new(r * a.cos() , r * a.sin() , z);
+    }
+
+    pub fn random_in_hemisphere(normal:Vec3) -> Vec3{
+        let in_unit_sphere:Vec3 = Vec3::random_in_unit_sphere();
+        if Vec3::dot(in_unit_sphere , normal) > 0.0 {
+            return in_unit_sphere;
+        }else {
+            return Vec3::new(0.0 , 0.0 , 0.0) - in_unit_sphere;
+        }
+    }
+    pub fn reflect(v:Vec3 , n:Vec3)->Vec3{
+        return v -  n * 2.0 * Vec3::dot(v , n) ;
+    }
+
+    pub fn refract(uv:Vec3 , n:Vec3 , etai_over_etat:f64)-> Vec3{
+        let cos_theta = Vec3::dot(Vec3::new(0.0 , 0.0 , 0.0) - uv , n);
+        let r_out_perp:Vec3 = (uv + n * cos_theta) * etai_over_etat;
+        let r_out_parallel = n * (- ((1.0 - r_out_perp.length_squared()).abs().sqrt()));
+        return r_out_perp + r_out_parallel;
+    }
+
+    pub fn random_in_unit_disk()->Vec3{
+        while true {
+            let p:Vec3 = Vec3::new(random_double_lim(-1.0 , 1.0) , random_double_lim(-1.0 , 1.0) , 0.0);
+            if p.length_squared() >= 1.0 {
+                continue;
+            }
+            return p;
+        }
+        return Vec3::new(0.0 , 0.0 , 0.0);
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -91,7 +334,7 @@ mod tests {
         )
     }
 
-    /*
+
     #[test]
     fn test_add_assign_f64() {
         let mut x = Vec3::new(1.0, 0.0, -1.0);
@@ -168,14 +411,14 @@ mod tests {
     fn test_neg() {
         assert_eq!(-Vec3::new(1.0, -2.0, 3.0), Vec3::new(-1.0, 2.0, -3.0));
     }
-    */
+
 
     #[test]
     fn test_squared_length() {
         assert_eq!(Vec3::new(1.0, 2.0, 3.0).squared_length(), 14.0_f64);
     }
 
-    /*
+
     #[test]
     fn test_length() {
         assert_eq!(
@@ -192,7 +435,7 @@ mod tests {
             Vec3::new(-1.0, 0.0, 0.0)
         );
     }
-
+    /*
     #[test]
     #[should_panic]
     fn test_unit_panic() {
